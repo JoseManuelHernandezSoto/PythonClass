053407533d934d9d1b4437faa0eb6564135212c6
JoseManuelHernandezSoto <152338114+JoseManuelHernandezSoto@users.noreply.github.com>
2024-04-16 22:23:18 -0700

    CorreccionDeMinMax
    Se corrigio el codigo, ahora es mas eficiente al momento de adivinar el numero

--- guess_number_inverse.py
+++ guess_number_inverse.py
@@ -1,19 +1,27 @@
 #Importamos la libreria random que sera muy importante
 import random
 # Creamos la variable 'number' y le asignamos un numero del 1 al 50 al azar
-number = random.randrange(1,50)
+min = 1
+max = 100
+number = random.randrange(min,max)
 # Pedimos al usuario ingresar un numero para que adivine la maquina
-guess = int(input('Ingresa un numero para que adivine la maquina (debe de ser entre 1 - 50)'))
+guess = int(input('Ingresa un numero para que adivine la maquina (debe de ser entre {} - {}): '.format(min,max)))
 count = 0
 ###
 ### Ciclo de adivinansa
 ###
 while guess != number:
-    if guess<1 or guess>50:
-        guess = int(input('Ingresa un numero para que adivine la maquina (debe de ser entre 1 - 50)'))
+    if guess<min or guess>max:
+        guess = int(input('Ingresa un numero para que adivine la maquina (debe de ser entre {} - {})'.format(min,max)))
     else:
+        if number < guess:
+            min= number
+        elif number > guess:
+            max = number     
+
         count+= 1
-        number = random.randrange(1,50)
+        print(number)
+        number = random.randrange(min,max)
 
 # Cuando la maquina adivina el numero 
 String =  'La maquina tardo {} intentos en adivinar tu numero'.format(count)
